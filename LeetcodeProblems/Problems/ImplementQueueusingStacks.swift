77a985d4b90be570a864f1814b37217ff062046e
Igor Tomilin <it.goryan@mail.ru>
2023-02-09 23:50:46 +0300

    Some commit

---
+++ LeetcodeProblems/Problems/ImplementQueueusingStacks.swift
@@ -0,0 +1,34 @@
+//
+//  ImplementQueueusingStacks.swift
+//  LeetcodeProblems
+//
+//  Created by Igor Tomilin on 09.02.2023.
+//
+
+import Foundation
+
+struct ImplementQueueusingStacks {
+//    class MyQueue {
+//
+//        init() {
+//            
+//        }
+//        
+//        func push(_ x: Int) {
+//            
+//        }
+//        
+//        func pop() -> Int {
+//            
+//        }
+//        
+//        func peek() -> Int {
+//            
+//        }
+//        
+//        func empty() -> Bool {
+//            
+//        }
+//    }
+
+}
--- LeetcodeProblems/main.swift
+++ LeetcodeProblems/main.swift
@@ -6,4 +6,4 @@
 //
 
 import Foundation
-TargetSum().test()
+IsomorphicStrings().test()
